98889a13c72057d8361233808335c42278820b1b
ThienNguyenWork <phucthien04032002@gmail.com>
2026-09-09 14:34:46 +0700

    chore: update resume link
    Replace outdated Google Drive link with the latest version of the portfolio resume.

--- src/App.tsx
+++ src/App.tsx
@@ -950,7 +950,7 @@ export default function App() {
               
               <div className="mt-12 flex justify-center">
                 <motion.a
-                  href="https://drive.google.com/file/d/1u8H0YBchAhfnaR9_2iRgmJeOdPhkVr3-/view?usp=sharing"
+                  href="https://drive.google.com/file/d/1zAsW3SrDmUKUZwW8Zx678nl0KfoarkR3/view?usp=sharing"
                   target="_blank"
                   rel="noopener noreferrer"
                   whileHover={{ scale: 1.1, y: -5 }}
